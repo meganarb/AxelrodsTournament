9452db94d2607c4f699a381387a1998f26902056
meganarb <115191710+meganarb@users.noreply.github.com>
2024-07-03 13:08:44 -0400

    updated base strat to include points

--- BaseStrat.py
+++ BaseStrat.py
@@ -2,6 +2,7 @@ class Strategy:
 
     def __init__(self, name):
         self.name = name
+        self.points = 0
 
     ## will be changed for each child class of strategy
     def strategy(self):
