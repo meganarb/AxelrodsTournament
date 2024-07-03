0433b8e72b84ae4579bf70162b3ec59aea8045a0
meganarb <115191710+meganarb@users.noreply.github.com>
2024-07-03 13:14:30 -0400

    started making the methods for testing the strategies against one another

---
+++ StratTest.py
@@ -0,0 +1,11 @@
+rounds = 0
+strategies = []
+past1 = False
+past2 = False
+
+def Test():
+    ## overall testing everyone in the list of strats
+
+def FaceOff(strat1, strat2):
+    ## testing two individual strats against one another
+    ## returns the winning strat
