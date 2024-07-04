ebf3f3c46a68515ff6d2293a8c76436319c84ee8
meganarb <115191710+meganarb@users.noreply.github.com>
2024-07-04 13:13:37 -0400

    created method for iterating through all combinations of strategies to test them against one another

--- StratTest.py
+++ StratTest.py
@@ -1,9 +1,25 @@
-past1 = False
-past2 = False
+import Strategies
 
-def Test(rounds:int, players):
-    # overall testing everyone in the list of strats
 
+def Test(rounds: int, players: Strategies):  # iterates through all combinations of players
+
+    for i in range(len(players) - 1):
+        for j in range(i + 1, len(players)):
+            temp(rounds, players[i], players[j])
+    return players  # other methods will have changed their scores
+
+
+def temp(rounds: int, player1, player2):
+
+    zero = 0
+    # puts players against each other for x rounds
 def FaceOff(strat1, strat2):
+
+    zero = 0
     # testing two individual strats against one another
     # returns the winning strat
+
+
+p1 = Strategies.TitForTat("p1")
+p2 = Strategies.TitForTwoTats("p2")
+Test(0, [p1, p2, p1, p1, p1, p1])
